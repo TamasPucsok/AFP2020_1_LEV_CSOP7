9f7d7f142c49b8e9b6db011c38d30ba46982a022
TamasPucsok <65065748+TamasPucsok@users.noreply.github.com>
2021-01-10 13:25:14 +0100

    Update signin.php
    login.php_03

--- Project/Web/signin.php
+++ Project/Web/signin.php
@@ -11,3 +11,25 @@ $result = mysqli_query($con, $s);
 $result2=mysqli_query($con,$rank);
 $num = mysqli_num_rows($result);
 $row=mysqli_fetch_assoc($result2);
+		
+if($num==1 && $row['rank']=="user")
+{
+			$_SESSION['username']=$username;
+			echo '<script type="text/javascript">'; 
+			echo 'alert("A bejelentkezés sikeres volt!");'; 
+			echo 'window.location.href = "home.php";';
+			echo '</script>';
+			exit();
+}elseif($num==1 && $row['rank']=="admin")
+{
+			echo '<script type="text/javascript">'; 
+			echo 'alert("A bejelentkezés sikeres volt!");'; 
+			echo 'window.location.href = "adminsite.php";';
+			echo '</script>';
+}else
+{
+	echo '<script type="text/javascript">'; 
+	echo 'alert("Sikertelen bejelentkezés, a felhasználó név vagy jelszó nem megfelelő. Próbálja újra!");'; 
+	echo 'window.location.href = "index.html";';
+	echo '</script>';
+}
