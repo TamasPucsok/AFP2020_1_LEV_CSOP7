3f07343c83e7e931e1f673d3bf469a3cdd9f760f
TamasPucsok <65065748+TamasPucsok@users.noreply.github.com>
2021-01-10 12:48:04 +0100

    Update signin.php
    signin.php_02

--- Project/Web/signin.php
+++ Project/Web/signin.php
@@ -4,4 +4,10 @@ $con = mysqli_connect('localhost', 'root','123456');
 mysqli_select_db($con, 'news portal');
 $username=$_POST['username'];
 $password=$_POST['password'];
-		
+
+$s="select * from user where username ='$username' && password = '$password'";
+$rank="select * from properties where username='$username'";
+$result = mysqli_query($con, $s);
+$result2=mysqli_query($con,$rank);
+$num = mysqli_num_rows($result);
+$row=mysqli_fetch_assoc($result2);
