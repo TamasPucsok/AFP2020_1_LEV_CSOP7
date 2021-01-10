844b0591c13cf8b166db5053d1da0bfd7df94316
TamasPucsok <65065748+TamasPucsok@users.noreply.github.com>
2021-01-10 12:46:56 +0100

    Create signin.php
    signin.php_01

---
+++ Project/Web/signin.php
@@ -0,0 +1,7 @@
+﻿<?php
+session_start();
+$con = mysqli_connect('localhost', 'root','123456');
+mysqli_select_db($con, 'news portal');
+$username=$_POST['username'];
+$password=$_POST['password'];
+		
