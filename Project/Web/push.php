38f84f1315d2e06b0a85ed81519bb33a63f7bb0e
gyoriz90 <64286754+gyoriz90@users.noreply.github.com>
2021-01-17 02:40:40 +0100

    Update push.php
    Felhasználónév megadásra nincs szükség, továbbdobásra kerül az előző SESSION-ből.

--- Project/Web/push.php
+++ Project/Web/push.php
@@ -6,13 +6,11 @@ $con = mysqli_connect('localhost', 'root','123456');
 
 mysqli_select_db($con, 'news portal');
 
-$username=$_POST['username'];
+$username=$_SESSION['username'];
 $surname=$_POST['surname'];
 $lastname=$_POST['lastname'];
 $rank=$_POST['rank'];
 $gender=$_POST['gender'];
-
-
 
 $s = " select * from user where username = '$username'";
 
@@ -21,8 +19,8 @@ $num = mysqli_num_rows($result);
 
 if($num==0){
 	echo '<script type="text/javascript">'; 
-	echo 'alert("Nem jó felhasználónevet adott meg, próbálja újra!");'; 
-	echo 'window.location.href = "properties.php";';
+	echo 'alert("HIBA: Nincs az adatbázisban a felhasználó!");'; 
+	echo 'window.location.href = "index.html";';
 	echo '</script>';
 	exit();
 	
@@ -31,7 +29,7 @@ if($num==0){
 {
 	$reg= " insert into properties(surname, lastname, gender, username, rank) values ('$surname', '$lastname', '$gender', '$username', '$rank')";
 	mysqli_query($con, $reg);
-	
+	$_SESSION['username']=$username;
 	echo '<script type="text/javascript">'; 
 	echo 'alert("Adja meg a preferenciáit.");'; 
 	echo 'window.location.href = "interests.php";';
