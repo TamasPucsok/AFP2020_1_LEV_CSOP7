4e31fbef0ed18f5d645ca36fb3bfe23aac2dcc4f
gyoriz90 <64286754+gyoriz90@users.noreply.github.com>
2021-01-17 02:41:46 +0100

    Update upload2.php
    Felhasználó továbbdobása az előző SESSIONBŐL.

--- Project/Web/upload2.php
+++ Project/Web/upload2.php
@@ -5,7 +5,7 @@ $con = mysqli_connect('localhost', 'root','123456');
 
 mysqli_select_db($con, 'news portal');
 
-$username=$_POST['username'];
+$username=$_SESSION['username'];
 
 $life = FALSE;
 if(isset($_POST['life'])){
@@ -42,9 +42,9 @@ $s = " select * from interests where username = '$username'";
 $result = mysqli_query($con, $s);
 $num = mysqli_num_rows($result);
 
-if($num==1){
+if($num==0){
 	echo '<script type="text/javascript">'; 
-	echo 'alert("A felhasználó érdeklődési köre már korábban tárolva lett, próbáld meg újra más felhasználónévvel!");'; 
+	echo 'alert("HIBA: Nincs az adatbázisban a felhasználó!");'; 
 	echo 'window.location.href = "interests.php";';
 	echo '</script>';
 	exit();
@@ -54,7 +54,7 @@ if($num==1){
 	$reg= " insert into interests(username, life, business, culture, sport, tech, economy) values ('$username', '$life', '$business', '$culture', '$sport', '$tech', '$economy')";
 	mysqli_query($con, $reg);
 	echo '<script type="text/javascript">'; 
-	echo 'alert("Sikeres mentés!");'; 
+	echo 'alert("Sikeres mentés, jelentkezz be!");'; 
 	echo 'window.location.href = "index.html";';
 	echo '</script>';
 	
